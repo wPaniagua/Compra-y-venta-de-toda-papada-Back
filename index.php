506173b3a96480c6976ad343a06ef8b49e100551
Wilson <paniagua2015@hotmail.com>
2020-03-08 14:02:55 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,86 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-	<title>Proyecto ingenieria del sofware</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/fuciones.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <title>Principal</title>
 </head>
 <body>
-
-	<h1 class="text-center">Pruebas Conexion base de datos</h1>
-
-	<?php 
-		include_once 'class-conexion.php';
-		$conexion = new Conexion();
-
-		$sql = "SELECT * FROM persona";
-  		$resultado = $conexion->ejecutarConsulta($sql); 
-	?>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-3">
-				
-			</div>
-			<div class="col-6">
-
-				<table class="table table-striped table-hover">                     
-		          <tr>
-		            <th>Nombre</th>
-		            <th>Apellidos</th>
-		            <th>Correo </th>
-		            <th>Direccion</th>
-		          </tr>
-		          <?php while( $persona = $conexion->obtenerFila($resultado) ){ ?>
-		            <tr>
-		              <td><?php echo $persona["primerNombre"]." ".$persona["segundoNombre"] ; ?></td>
-		              <td><?php echo $persona["primerApellido"]." ".$persona["segundoApellido"] ; ?></td>
-		              <td><?php echo $persona["correo"] ; ?></td>
-		              <td><?php echo $persona["idMunicipio"] ; ?></td>
-		              <td>
-		                <a class="btn btn-danger" href="pedido.php?id=<?php echo $persona['ID'] ?>">Prueba</a> 
-		              </td>
-		            </tr>
-		          <?php } ?>
-		        </table>
-				
-			</div>
-			
-		</div>
-	</div>
-
-
-
+    <div class="container">
+        <!--Boton de registro-->
+        <button type="button" id="btnRegistro" class="btn btn-primary" data-toggle="modal" data-target="#modalRegistro">Registro</button><br>
+        <!--El Modal-->
+        <div class="modal fade" id="modalRegistro" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <!--Cabecera del Modal-->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Registro</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <!--Cuerpo del Modal-->
+                    <div class="modal-body">
+                        <form action="registro.php" method="POST" role="form">
+                            <input type="text" name="nombre" id="nombre" placeholder="Escriba su nombre" required>
+                            <input type="text" name="apellido" id="apellido" placeholder="Escriba su apellido" required><br><br>
+                            <img class="svg" src="ico/email.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="text" name="correo" id="correo" placeholder="Ingrese el correo" required><br><br>
+                            <img class="svg" src="ico/address-card-regular.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="number" name="ID" id="ID" placeholder="numero de identidad" required><br><br>
+                            <img class="svg" src="ico/telefono.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="number" name="telefono" id="telefono" placeholder="Ingrese su telefono" required><br><br>
+                            <img class="svg" src="ico/calendario.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="date" name="fechaNacmiento" id="fechaNacimineto"step="1" min="1940-01-01" max="2020-04-03" required><br><br>
+                            <img class="svg" src="ico/pin1.svg" alt="insertar SVG con la etiqueta image">
+                            <select name="depto" id="depto" required>
+                                <option value="">Seleccione: Depto</option>
+                            </select>
+                            <img class="svg" src="ico/pin.svg" alt="insertar SVG con la etiqueta image">
+                            <select name="municipio" id="municipio" required>
+                            <option value="">Seleccione: Municipio</option>
+                            </select><br><br>
+                            <img class="svg" src="ico/clave.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="password" name="contresenia" id="contrasenia" placeholder="escriba su contraseña" required
+                            minlength="6"><br><br>
+                            <img class="svg" src="ico/bloquear.svg" alt="insertar SVG con la etiqueta image">
+                            <input type="password" name="confContrasenia" id="confContrasenia" placeholder="Vuelva a escribir la contraseña" required><br><br>
+                            <div class="form-group">
+                                <fieldset>
+                                    <legend>Elija el tipo de usuario:</legend>
+                                    <label for="radio">
+                                        <input type="radio" name="tipo" id="tiposEmpresa" value="1">Empresa
+                                    </label>
+                                    <label for="radio">
+                                        <input type="radio" name="tipo" id="tipoComun" value="2" checked="checked">Comprador/Vendedor
+                                    </label>
+                                </fieldset>
+                            </div>
+                            <input type="checkbox" name="contrato" id="contrtato" required>Acepta nuestro termino y condiciones
+                            <div id="exito" style="display:none">
+                                Sus datos han sido recibidos con éxito.
+                            </div>
+                            <div id="fracaso" style="display:none">
+                                Se ha producido un error durante el envío de datos.
+                            </div>
+                        </form>
+                    </div>
+                    <!--Pie del Modal-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="button" id="btnGuardar" class="btn btn-primary submitBtn">Guardar</button>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
 </body>
+<script src="js/funciones.js"></script>
 </html>
