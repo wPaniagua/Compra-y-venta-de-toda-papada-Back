1e347859949734e3c1721ff82e9370a471cbac16
Saulg96 <41869878+Saulg96@users.noreply.github.com>
2020-03-16 18:44:14 -0600

    Update verUsuariosDesdeAdmin.js
    Se agrego la funcionalidad de filtros

--- js/verUsuariosDesdeAdmin.js
+++ js/verUsuariosDesdeAdmin.js
@@ -1,21 +1,20 @@
 $(window).on("load", function () {
     cargarTabla();
-    //cargarTipoUsuarios();
+    cargarTipoUsuarios();
     cargarDepartamento();
     cargarMunicipio();
 
 })
-
-
+var respuestaTbl;
+var busqueda;
+/**==============================================================CARGAR TABLA EN GENERAL======================================================= */
 function darDeBaja(idPersona) {
 
     $.ajax({
-        url: "backend/verUsuariosDesdeAdmin.php",
-        data: `idPersona=${idPersona}&accion=darDeBaja`,
-        method: "POST",
+        url: `backend/verUsuariosDesdeAdmin.php?idPersona=${idPersona}&action=darDeBaja`,
+        method: "GET",
+        dataType: "json",
         success: function (respuesta) {
-
-            // console.log(JSON.parse(respuesta))
 
             console.log(respuesta)
 
@@ -34,8 +33,7 @@ function cargarTabla() {
         method: "GET",
         dataType: "json",
         success: function (respuesta) {
-            //let respuesta =JSON.stringify(respuestas);
-            //let response = JSON.parse(respuesta)
+
             console.log(respuesta);
 
             $("#tablaUsuarios>tbody").html(" ");
@@ -65,25 +63,140 @@ function cargarTabla() {
         }
     });
 }
+/**==============================================================FIN CARGAR TABLA EN GENERAL======================================================= */
 
-$("#slc-categorias").on("change", function () {
+
+/**==============================================================CARGAR TBL======================================================= */
+function darDeBajaTbl(idPersona) {
+
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?idPersona=${idPersona}&action=darDeBaja`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+
+}
+
+function cargarTablaTbl() {
+    
+    $("#tablaUsuarios>tbody").html(" ");
+
+    for (let i = 0; i < respuestaTbl.length; i++) {
+
+        $("#tablaUsuarios>tbody").append(
+            `<tr>  
+        <td>${respuestaTbl[i].primerNombre}</td>
+        <td>${respuestaTbl[i].primerApellido}</td>
+        <td>${respuestaTbl[i].tipoUsuario}</td>
+        <td>${respuestaTbl[i].fechaNacimiento}</td>
+        <td>${respuestaTbl[i].telefono}</td>
+        <td>${respuestaTbl[i].correo}</td>
+        <td>${respuestaTbl[i].departamento}</td>
+        <td>${respuestaTbl[i].municipio}</td>
+        <td>${respuestaTbl[i].estado}</td>
+        <td><button class="btn btn-outline-danger" onclick="darDeBajaTbl(${respuestaTbl[i].idPersona})"><i class="far fa-trash-alt"></i></button></td>
+        </tr>`
+        );
+
+    }
+
+}
+/**==============================================================FIN CARGAR TBL ======================================================= */
+
+/**==============================================================BUSQUEDA======================================================= */
+$("#inputBusquedaBtn").click(function () {
+    //var optionSelected = $("option:selected", this);
+    var valueSelected = $("#inputBusqueda").val();
+    busqueda=valueSelected;
+    console.log(valueSelected);
+    hacerBusqueda();
+    /*$.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?palabraClave=${valueSelected}&action=busquedaNombreUsuario`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });*/
+
+});
+
+function hacerBusqueda(){
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?palabraClave=${busqueda}&action=busquedaNombreUsuario`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+}
+
+/*$('#inputBusqueda').keypress(function(e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+        hacerBusqueda();
+        //Buscar();
+        //e.preventDefault();
+        //return false;
+    }
+});*//////////////////////////////////////////////////////////////////////
+/*function myFunction(e) {
+    //See notes about 'which' and 'key'
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == 13) {
+        hacerBusqueda();
+    }
+}*/
+/**==============================================================FIN BUSQUEDA======================================================= */
+
+
+/**==============================================================SELECCIONAR TIPO DE USUARIO======================================================= */
+$("#slc-tipoUsuario").on("change", function () {
     var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
 
     console.log(valueSelected);
 
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?idUsuario=${valueSelected}&action=filtrarUsuario`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
 });
-
 
 function cargarTipoUsuarios() {
     $.ajax({
-        url: "backend/verUsuariosDesdeAdmin.php",
-        method: "POST",
-        data: `accion=seleccionarTipoUsuarios`,
-        success: function (respuesta) {
-            let response = JSON.parse(respuesta);
-
-            // $("#slc-categorias").html(" ");
+        url: "backend/verUsuariosDesdeAdmin.php?action=seleccionarTipoUsuarios",
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
 
             for (let i = 0; i < response.length; i++) {
                 $("#slc-tipoUsuario").append(
@@ -96,6 +209,31 @@ function cargarTipoUsuarios() {
         }
     });
 }
+/**==============================================================FIN SELECCIONAR TIPO DE USUARIO======================================================= */
+
+
+/**==============================================================SELECCIONAR DEPARTAMENTO======================================================= */
+$("#slc-departamento").on("change", function () {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    console.log(valueSelected);
+
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?idDepto=${valueSelected}&action=filtrarDepto`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+});
 
 function cargarDepartamento() {
     $.ajax({
@@ -103,10 +241,6 @@ function cargarDepartamento() {
         method: "GET",
         dataType: "json",
         success: function (respuesta) {
-            //let response = JSON.parse(respuesta);
-
-            //console.log(response);
-            // $("#slc-categorias").html(" ");
 
             for (let i = 0; i < respuesta.length; i++) {
                 $("#slc-departamento").append(
@@ -119,6 +253,32 @@ function cargarDepartamento() {
         }
     });
 }
+/**==============================================================FIN SELECCIONAR DEPARTAMENTO======================================================= */
+
+
+/**==============================================================SELECCIONAR MUNICIPIO======================================================= */
+
+$("#slc-municipio").on("change", function () {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    console.log(valueSelected);
+
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?idMunicipio=${valueSelected}&action=filtrarMunicipio`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+});
 
 function cargarMunicipio() {
     $.ajax({
@@ -126,10 +286,6 @@ function cargarMunicipio() {
         method: "GET",
         dataType: "json",
         success: function (respuesta) {
-            //let response = JSON.parse(respuesta);
-
-            // $("#slc-categorias").html(" ");
-
             for (let i = 0; i < respuesta.length; i++) {
                 $("#slc-municipio").append(
                     `<option value=${respuesta[i].idMunicipio}>${respuesta[i].nombre}</option>`
@@ -141,3 +297,30 @@ function cargarMunicipio() {
         }
     });
 }
+/**==============================================================FIN SELECCIONAR MUNICIPIO======================================================= */
+
+/**==============================================================SELECCIONAR ESTADO DEL USUARIO======================================================= */
+
+$("#slc-estados").on("change", function () {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    console.log(valueSelected);
+
+    $.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?estado=${valueSelected}&action=filtrarEstado`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+});
+
+/**==============================================================FIN SELECCIONAR ESTADO DEL USUARIO======================================================= */
