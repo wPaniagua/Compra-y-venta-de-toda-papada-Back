3ce04084e12901ec52b1a57cf0824ade1d97a57c
Saulg96 <41869878+Saulg96@users.noreply.github.com>
2020-03-16 18:42:58 -0600

    Update publicaciones-admin.js
    Se agrego la funcionalidad de filtros

--- js/publicaciones-admin.js
+++ js/publicaciones-admin.js
@@ -5,13 +5,15 @@ $(window).on("load", function () {
 
 })
 
-
+var respuestaTbl;
+var busqueda;
+/**==============================================================CARGAR TABLA EN GENERAL======================================================= */
 function darDeBaja(idAnuncio) {
 
     $.ajax({
-        url: "backend/publicaciones_admin.php",
-        data: `idAnuncio=${idAnuncio}&accion=darDeBaja`,
-        method: "POST",
+        url: `backend/publicaciones_admin.php?idAnuncio=${idAnuncio}&accion=darDeBaja`,
+        method: "GET",
+        dataType: "json",
         success: function (respuesta) {
 
             // console.log(JSON.parse(respuesta))
@@ -29,12 +31,12 @@ function darDeBaja(idAnuncio) {
 
 function cargarTabla() {
     $.ajax({
-        url: "backend/publicaciones_admin.php",
-        data: `accion=getPublicaciones`,
-        method: "POST",
-        success: function (respuesta) {
+        url: "backend/publicaciones_admin.php?accion=getPublicaciones",
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
 
-            let response = JSON.parse(respuesta)
+            //let response = JSON.parse(respuesta)
             console.log(response.length);
 
             $("#tablaPublicaciones>tbody").html(" ");
@@ -65,29 +67,144 @@ function cargarTabla() {
         }
     });
 }
+/**==============================================================FIN CARGAR TABLA EN GENERAL======================================================= */
 
+/**==============================================================CARGAR TBL======================================================= */
+function darDeBajaTbl(idAnuncio) {
+
+    $.ajax({
+        url: `backend/publicaciones_admin.php?idAnuncio=${idAnuncio}&accion=darDeBaja`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+
+}
+
+function cargarTablaTbl() {
+    
+    $("#tablaPublicaciones>tbody").html(" ");
+
+    for (let i = 0; i < respuestaTbl.length; i++) {
+
+        $("#tablaPublicaciones>tbody").append(
+            `<tr>  
+            <td>${respuestaTbl[i].nombreProducto}</td>
+            <td>${respuestaTbl[i].tipoProducto}</td>
+            <td>${respuestaTbl[i].categoria}</td>
+            <td>${respuestaTbl[i].descripcion}</td>
+            <td>${respuestaTbl[i].primerNombre} ${respuestaTbl[i].primerApellido}</td>
+            <td>${respuestaTbl[i].precio}</td>
+            <td>${respuestaTbl[i].moneda}</td>
+            <td>${respuestaTbl[i].fechaPublicacion}</td>
+            <td>${respuestaTbl[i].fechaVencimiento}</td>
+            <td>${respuestaTbl[i].estado}</td>
+            <td><button class="btn btn-outline-danger" onclick="darDeBajaTbl(${respuestaTbl[i].idAnuncio})"><i class="far fa-trash-alt"></i></button></td>
+            </tr>`
+        );
+
+    }
+
+}
+/**==============================================================FIN CARGAR TBL ======================================================= */
+
+/**==============================================================BUSQUEDA======================================================= */
+$("#inputBusquedaBtn").click(function () {
+    //var optionSelected = $("option:selected", this);
+    var valueSelected = $("#inputBusqueda").val();
+    busqueda=valueSelected;
+    console.log(valueSelected);
+    hacerBusqueda();
+    /*$.ajax({
+        url: `backend/verUsuariosDesdeAdmin.php?palabraClave=${valueSelected}&action=busquedaNombreUsuario`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });*/
+
+});
+
+function hacerBusqueda(){
+    $.ajax({
+        url: `backend/publicaciones_Admin.php?palabraClave=${busqueda}&accion=busquedaNombreAnuncio`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+}
+
+/*$('#inputBusqueda').keypress(function(e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+        hacerBusqueda();
+        //Buscar();
+        //e.preventDefault();
+        //return false;
+    }
+});*//////////////////////////////////////////////////////////////////////
+/*function myFunction(e) {
+    //See notes about 'which' and 'key'
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == 13) {
+        hacerBusqueda();
+    }
+}*/
+/**==============================================================FIN BUSQUEDA======================================================= */
+
+/**==============================================================SELECCIONAR CATEGORIA======================================================= */
 $("#slc-categorias").on("change", function () {
     var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
 
     console.log(valueSelected);
 
+    $.ajax({
+        url: `backend/publicaciones_Admin.php?idCategoria=${valueSelected}&accion=filtrarCategoria`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+
 });
 
 
 function cargarCategorias() {
     $.ajax({
-        url: "backend/publicaciones_admin.php",
-        method: "POST",
-        data: `accion=seleccionarCategorias`,
-        success: function (respuesta) {
-            let response = JSON.parse(respuesta);
-
-            // $("#slc-categorias").html(" ");
-
+        url: "backend/publicaciones_admin.php?accion=seleccionarCategorias",
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
             for (let i = 0; i < response.length; i++) {
                 $("#slc-categorias").append(
-                    `<option value=${response[i].idCategoria}>${response[i].categoria}</option>`
+                    `<option value=${response[i].idCategorias}>${response[i].nombreCategoria}</option>`
                 )
             }
         },
@@ -96,14 +213,38 @@ function cargarCategorias() {
         }
     });
 }
+/**==============================================================FIN SELECCIONAR CATEGORIA======================================================= */
+
+/**==============================================================SELECCIONAR USUARIO======================================================= */
+$("#slc-usuarios").on("change", function () {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    console.log(valueSelected);
+
+    $.ajax({
+        url: `backend/publicaciones_Admin.php?idUsuario=${valueSelected}&accion=filtrarUsuario`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+
+});
 
 function cargarUsuarios() {
     $.ajax({
-        url: "backend/publicaciones_admin.php",
-        method: "POST",
-        data: `accion=seleccionarUsuarios`,
-        success: function (respuesta) {
-            let response = JSON.parse(respuesta);
+        url: "backend/publicaciones_admin.php?accion=seleccionarUsuarios",
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
+            //let response = JSON.parse(respuesta);
 
             console.log(response);
             // $("#slc-categorias").html(" ");
@@ -119,3 +260,30 @@ function cargarUsuarios() {
         }
     });
 }
+/**==============================================================FIN SELECCIONAR USUARIO======================================================= */
+
+/**==============================================================SELECCIONAR ESTADO DEL ANUNCIO======================================================= */
+
+$("#slc-estados").on("change", function () {
+    var optionSelected = $("option:selected", this);
+    var valueSelected = this.value;
+
+    console.log(valueSelected);
+
+    $.ajax({
+        url: `backend/publicaciones_admin.php?estado=${valueSelected}&accion=filtrarEstado`,
+        method: "GET",
+        dataType: "json",
+        success: function (respuesta) {
+
+            respuestaTbl=respuesta;
+            console.log(respuesta)
+            cargarTablaTbl();
+        },
+        error: function (respuesta) {
+            console.log(error);
+        }
+    });
+});
+
+/**==============================================================FIN SELECCIONAR ESTADO DEL ANUNCIO======================================================= */
