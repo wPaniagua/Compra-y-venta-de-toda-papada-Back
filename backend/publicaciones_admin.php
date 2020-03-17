945a0b60b107b7f6e7275246f593b94343748b76
Anthony Guevara <anthony_guevara@lumminatech.com>
2020-03-17 12:18:46 -0600

    Pagina de publicaciones admin, cambio de columna en la base de datos

--- backend/publicaciones_admin.php
+++ backend/publicaciones_admin.php
@@ -2,7 +2,7 @@
 
 $mysqli = new mysqli( 'localhost:3308', 'root', '', 'mydb' );
 
-switch ($_GET["accion"]) {
+switch ($_POST["accion"]) {
 
     case "getPublicaciones":
 
@@ -35,7 +35,7 @@ switch ($_GET["accion"]) {
         case "darDeBaja":
 
 
-            $idAnuncio = $_GET["idAnuncio"];
+            $idAnuncio = $_POST["idAnuncio"];
 
             $stmt = $mysqli -> prepare('UPDATE anuncios  SET estado = "I"  WHERE idAnuncios = ?');
             $stmt->bind_param('i', $idAnuncio);
@@ -46,14 +46,14 @@ switch ($_GET["accion"]) {
             // $stmt -> fetch();
 
 
-            echo (json_encode(array("idAnuncio"=>$_GET["idAnuncio"])));
+            echo (json_encode(array("idAnuncio"=>$_POST["idAnuncio"])));
 
         break;
 
         case "seleccionarCategorias":
 
-            $idCategorias;
-            $descripcion;
+            $idCategoria;
+            $categoria;
 
             $stmt = $mysqli -> prepare(
                 'SELECT idCategorias, descripcion FROM categorias'
@@ -62,8 +62,8 @@ switch ($_GET["accion"]) {
             $stmt -> execute();
             $stmt -> store_result();
             $stmt -> bind_result( 
-                $idCategorias,
-                $descripcion);
+                $idCategoria,
+                $categoria);
             
             
             $respuesta = array();
@@ -73,8 +73,8 @@ switch ($_GET["accion"]) {
             while($stmt -> fetch()){
             
                 $respuesta[$index] =  array(
-                    "idCategorias"=>$idCategorias,
-                    "nombreCategoria"=>$descripcion
+                    "idCategoria"=>$idCategoria,
+                    "categoria"=>$categoria,
                 );
             
                 $index++;
@@ -117,242 +117,190 @@ switch ($_GET["accion"]) {
             }
 
             echo json_encode($respuesta);
-        break;  
+        break;   
         
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        case "busquedaNombreAnuncio":
+        
+        case "getPublicacionesNingunaNull":
 
-        $palabraClave = $_GET["palabraClave"];
-        $palabra="%$palabraClave%";
-        $stmt = $mysqli -> prepare(
-            'SELECT  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
-            an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
-            inner join producto pro on an.idProducto=pro.idProducto
-            inner join categorias ca on pro.idCategorias=ca.idCategorias
-            inner join persona per on an.idPersona=per.idPersona
-            inner join moneda mo on an.idMoneda=mo.idMoneda  WHERE an.descripcion LIKE ?');
-        $stmt->bind_param('s', $palabra);  
-        $stmt -> execute();
-        $stmt -> store_result();
-        $stmt -> bind_result( 
-            $nombreProducto,
-            $tipoProducto,
-            $categoria,
-            $descripcion,
-            $primerNombre,
-            $primerApellido,
-            $precio,
-            $moneda,
-            $fechaPublicacion,
-            $fechaVencimiento,
-            $estado,
-            $idAnuncio
-            );
+            
+            $idPersona= $_POST["usuario"];
+            $nombreProducto;
+            $tipoProducto;
+            $categoria =  $_POST["categoria"];
+            $descripcion;
+            $primerNombre;
+            $primerApellido;
+            $precio;
+            $moneda;
+            $fechaPublicacion;
+            $fechaVencimiento;
+            $estado = $_POST["estado"];
+            $idAnuncio; 
         
         
-        $respuesta = array();
-        
-        $index = 0;
-        
-        while($stmt -> fetch()){
-        
-            $respuesta[$index] =  array(
-                "nombreProducto"=>$nombreProducto,
-                "tipoProducto"=>$tipoProducto,
-                "categoria"=>$categoria,
-                "descripcion"=>$descripcion,
-                "primerNombre"=>$primerNombre,
-                "primerApellido"=>$primerApellido,
-                "precio"=>$precio,
-                "moneda"=>$moneda,
-                "fechaPublicacion"=>$fechaPublicacion,
-                "fechaVencimiento"=>$fechaVencimiento,
-                "estado"=>$estado,
-                "idAnuncio"=>$idAnuncio
-            );
-        
-            $index++;
-        }
-        
-        echo json_encode($respuesta);
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE per.idPersona = ? AND an.estado LIKE  ?  AND ca.idCategorias = ?');
+
+            $stmt->bind_param('isi', $idPersona, $estado, $categoria);
+
+            getAnuncios($stmt);
 
         break;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                
+        case "getPublicacionesEstadoNull":
+            $idPersona= $_POST["usuario"];
+            $nombreProducto;
+            $tipoProducto;
+            $categoria =  $_POST["categoria"];
+            $descripcion;
+            $primerNombre;
+            $primerApellido;
+            $precio;
+            $moneda;
+            $fechaPublicacion;
+            $fechaVencimiento;
+            $idAnuncio; 
+        
+        
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE per.idPersona = ? AND ca.idCategorias = ?');
 
+            $stmt->bind_param('ii', $idPersona, $categoria);
 
-        
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        case "filtrarCategoria":
-
-        $idCategoria = $_GET["idCategoria"];
-        $stmt = $mysqli -> prepare(
-            'SELECT  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
-            an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
-            inner join producto pro on an.idProducto=pro.idProducto
-            inner join categorias ca on pro.idCategorias=ca.idCategorias
-            inner join persona per on an.idPersona=per.idPersona
-            inner join moneda mo on an.idMoneda=mo.idMoneda WHERE ca.idCategorias = ?');
-        $stmt->bind_param('i', $idCategoria);  
-        $stmt -> execute();
-        $stmt -> store_result();
-        $stmt -> bind_result( 
-            $nombreProducto,
-            $tipoProducto,
-            $categoria,
-            $descripcion,
-            $primerNombre,
-            $primerApellido,
-            $precio,
-            $moneda,
-            $fechaPublicacion,
-            $fechaVencimiento,
-            $estado,
-            $idAnuncio
-            );
-        
-        
-        $respuesta = array();
-        
-        $index = 0;
-        
-        while($stmt -> fetch()){
-        
-            $respuesta[$index] =  array(
-                "nombreProducto"=>$nombreProducto,
-                "tipoProducto"=>$tipoProducto,
-                "categoria"=>$categoria,
-                "descripcion"=>$descripcion,
-                "primerNombre"=>$primerNombre,
-                "primerApellido"=>$primerApellido,
-                "precio"=>$precio,
-                "moneda"=>$moneda,
-                "fechaPublicacion"=>$fechaPublicacion,
-                "fechaVencimiento"=>$fechaVencimiento,
-                "estado"=>$estado,
-                "idAnuncio"=>$idAnuncio
-            );
-        
-            $index++;
-        }
-        
-        echo json_encode($respuesta);
+            getAnuncios($stmt);
 
         break;
 
-        case "filtrarUsuario":
+        case "getPublicacionesUsuarioNull":
+            $idPersona;
+            $nombreProducto;
+            $tipoProducto;
+            $categoria =  $_POST["categoria"];
+            $descripcion;
+            $primerNombre;
+            $primerApellido;
+            $precio;
+            $moneda;
+            $fechaPublicacion;
+            $fechaVencimiento;
+            $idAnuncio; 
+            $estado = $_POST["estado"];
+        
+        
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE an.estado LIKE ? AND ca.idCategorias = ?');
 
-        $idUsuario = $_GET["idUsuario"];
-        $stmt = $mysqli -> prepare(
-            'SELECT  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
-            an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
-            inner join producto pro on an.idProducto=pro.idProducto
-            inner join categorias ca on pro.idCategorias=ca.idCategorias
-            inner join persona per on an.idPersona=per.idPersona
-            inner join moneda mo on an.idMoneda=mo.idMoneda WHERE per.idPersona = ?');
-        $stmt->bind_param('i', $idUsuario);  
-        $stmt -> execute();
-        $stmt -> store_result();
-        $stmt -> bind_result( 
-            $nombreProducto,
-            $tipoProducto,
-            $categoria,
-            $descripcion,
-            $primerNombre,
-            $primerApellido,
-            $precio,
-            $moneda,
-            $fechaPublicacion,
-            $fechaVencimiento,
-            $estado,
-            $idAnuncio
-            );
+            $stmt->bind_param('si', $estado, $categoria);
+
+            getAnuncios($stmt);
+    
+        break;
+
+        case "getCategoriasNull":
+            $idPersona = $_POST["usuario"];
+            $nombreProducto;
+            $tipoProducto;
+            $categoria;
+            $descripcion;
+            $primerNombre;
+            $primerApellido;
+            $precio;
+            $moneda;
+            $fechaPublicacion;
+            $fechaVencimiento;
+            $idAnuncio; 
+            $estado = $_POST["estado"];
         
         
-        $respuesta = array();
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE an.estado LIKE ? AND per.idPersona = ?');
+
+            $stmt->bind_param('si', $estado, $idPersona);
+
+            getAnuncios($stmt);
+        break;
+
+        case "getPublicacionesSoloEstado":
+
+            $estado = $_POST["estado"];
         
-        $index = 0;
         
-        while($stmt -> fetch()){
-        
-            $respuesta[$index] =  array(
-                "nombreProducto"=>$nombreProducto,
-                "tipoProducto"=>$tipoProducto,
-                "categoria"=>$categoria,
-                "descripcion"=>$descripcion,
-                "primerNombre"=>$primerNombre,
-                "primerApellido"=>$primerApellido,
-                "precio"=>$precio,
-                "moneda"=>$moneda,
-                "fechaPublicacion"=>$fechaPublicacion,
-                "fechaVencimiento"=>$fechaVencimiento,
-                "estado"=>$estado,
-                "idAnuncio"=>$idAnuncio
-            );
-        
-            $index++;
-        }
-        
-        echo json_encode($respuesta);
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE an.estado LIKE ?');
+
+            $stmt->bind_param('s', $estado);
+
+            getAnuncios($stmt);
 
         break;
 
-        case "filtrarEstado":
+        case "getPublicacionesSoloUsuario":
 
-        $estado = $_GET["estado"];
-        $stmt = $mysqli -> prepare(
-            'SELECT  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
-            an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
-            inner join producto pro on an.idProducto=pro.idProducto
-            inner join categorias ca on pro.idCategorias=ca.idCategorias
-            inner join persona per on an.idPersona=per.idPersona
-            inner join moneda mo on an.idMoneda=mo.idMoneda WHERE an.estado = ?');
-        $stmt->bind_param('s', $estado);  
-        $stmt -> execute();
-        $stmt -> store_result();
-        $stmt -> bind_result( 
-            $nombreProducto,
-            $tipoProducto,
-            $categoria,
-            $descripcion,
-            $primerNombre,
-            $primerApellido,
-            $precio,
-            $moneda,
-            $fechaPublicacion,
-            $fechaVencimiento,
-            $estado,
-            $idAnuncio
-            );
+            $idPersona = $_POST["usuario"];
         
         
-        $respuesta = array();
-        
-        $index = 0;
-        
-        while($stmt -> fetch()){
-        
-            $respuesta[$index] =  array(
-                "nombreProducto"=>$nombreProducto,
-                "tipoProducto"=>$tipoProducto,
-                "categoria"=>$categoria,
-                "descripcion"=>$descripcion,
-                "primerNombre"=>$primerNombre,
-                "primerApellido"=>$primerApellido,
-                "precio"=>$precio,
-                "moneda"=>$moneda,
-                "fechaPublicacion"=>$fechaPublicacion,
-                "fechaVencimiento"=>$fechaVencimiento,
-                "estado"=>$estado,
-                "idAnuncio"=>$idAnuncio
-            );
-        
-            $index++;
-        }
-        
-        echo json_encode($respuesta);
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE per.idPersona = ?');
 
+            $stmt->bind_param('i', $idPersona);
+
+            getAnuncios($stmt);
         break;
+
+        case "getPublicacionesSoloCategoria":
+            $categoria = $_POST["categoria"];
+        
+        
+            $stmt = $mysqli -> prepare(
+                'select  pro.nombre,pro.tipoProducto,ca.descripcion categoria,an.descripcion,per.primerNombre,per.primerApellido ,an.precio,mo.descripcion ,an.fechaPublicacion,
+                an.fechaVencimiento,an.estado, an.idAnuncios from anuncios an
+                inner join producto pro on an.idProducto=pro.idProducto
+                inner join categorias ca on pro.idCategorias=ca.idCategorias
+                inner join persona per on an.idPersona=per.idPersona
+                inner join moneda mo on an.idMoneda=mo.idMoneda
+                WHERE pro.idCategorias = ?');
+
+            $stmt->bind_param('i', $categoria);
+
+            getAnuncios($stmt);
+        break;
+
 
 }
 
@@ -421,9 +369,4 @@ function getAnuncios($stmt){
 }
 
 //works
-
-/**
- * 
- * 
- */
 ?>
