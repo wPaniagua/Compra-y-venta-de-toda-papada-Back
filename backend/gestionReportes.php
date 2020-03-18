1bd62f5856eba418fb47826e6539a9a9e672ec07
IrisWaleska <alvaradoisis22@gmail.com>
2020-03-18 17:58:33 -0600

    reportes

---
+++ backend/gestionReportes.php
@@ -0,0 +1,86 @@
+<?php
+	
+	$mysqli = new mysqli("localhost", "root", "", "mydb");
+
+	
+	switch($_GET["accion"]){
+
+		case 'obtenerDenuncias':
+			
+			$mysqli->multi_query("SET @p0='obtenerDenuncias'; SET @p1=''; CALL SP_REPORTES(@p0, @p1);");
+			$resultadoConsulta = array();
+			do {
+			    if ($resultado = $mysqli->store_result()) {
+					  while ($fila = $resultado->fetch_assoc()) {
+
+        				$resultadoConsulta[] = $fila;
+
+			            }
+			        
+			        $resultado->free();
+			    } else {
+			        if ($mysqli->errno) {
+			            echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			        }
+			    }
+			} while ($mysqli->more_results() && $mysqli->next_result());
+
+			echo json_encode($resultadoConsulta);
+	
+		break;
+					
+
+		case 'obtenerUsuarios':
+			$mysqli->multi_query("SET @p0='obtenerUsuarios'; SET @p1=''; CALL SP_REPORTES(@p0, @p1);");
+			$resultadoConsulta = array();
+			do {
+			    if ($resultado = $mysqli->store_result()) {
+					  while ($fila = $resultado->fetch_assoc()) {
+
+        				$resultadoConsulta[] = $fila;
+
+			            }
+			        
+			        $resultado->free();
+			    } else {
+			        if ($mysqli->errno) {
+			            echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			        }
+			    }
+			} while ($mysqli->more_results() && $mysqli->next_result());
+
+			echo json_encode($resultadoConsulta);
+	
+		break;
+
+		
+		
+		case 'buscarNombre':
+			$nombreRepo=$_GET["nombreRepo"];
+			//$nombreCat="Camas";
+			$resultadoConsulta = array();
+			$mysqli->multi_query("SET @p0='".$nombreRepo."'; SET @p1=''; SET @p2='obtenerPorPalabra'; SET @p3=''; CALL `SP_REPORTES`(@p0, @p1, @p2, @p3, @p4); SELECT @p4 AS `mensaje`;");
+
+			$resultadoConsulta = array();
+			do {
+			    if ($resultado = $mysqli->store_result()) {
+					  while ($fila = $resultado->fetch_assoc()) {
+
+        				$resultadoConsulta[] = $fila;
+
+			            }
+			        
+			        $resultado->free();
+			    } else {
+			        if ($mysqli->errno) {
+			            echo "Store failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			        }
+			    }
+			} while ($mysqli->more_results() && $mysqli->next_result());
+
+			echo json_encode($resultadoConsulta);
+					
+		break;
+
+	}
+?>
