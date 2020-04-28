ba2377b026f71c047baf564d826fb289097eb684
Anthony Guevara <anthony_guevara@lumminatech.com>
2020-04-28 16:58:49 -0600

    Notificacion por correo a los favoritos al momento de agregar una publicacion

--- backend/gestionAgregarPub.php
+++ backend/gestionAgregarPub.php
@@ -1,6 +1,10 @@
 <?php 
 //session_start(); 
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'C:\wamp64\composer\vendor\autoload.php';
+
 $mysqli = new mysqli( 'localhost:3306', 'root', '', 'mydb' );
 
 //$prueba = "registrarAnuncio";
@@ -78,6 +82,92 @@ switch ($_POST["accion"]) {
 } while ($mysqli->more_results() && $mysqli->next_result());
 
 echo json_encode($resultadoConsulta);
+
+//enviar el correo a todos los favoritos del usuario
+
+    //creo la instancia de la extension
+    $mail = new PHPMailer(TRUE);
+
+
+    $call = $mysqli->prepare(
+    'SELECT per.correo correoFavorito, per.primerNombre  pnFavorito, per.primerApellido  paFavorito, 
+    p.correo correoUsuario,p.primerNombre pnUsuario, p.primerApellido paUsuario FROM favoritos f 
+    INNER JOIN persona per ON per.idPersona = f.favorito 
+    INNER JOIN persona p on p.idPersona =  f.idPersona 
+    WHERE f.favorito =  ?');
+
+    $call->bind_param('i',$idPersona );
+    $call->execute();
+    $result = $call->get_result();
+    //$result = $stmt->get_result(); 
+
+    while ($row = $result->fetch_assoc()) {
+
+        $cuerpo_mail = "
+        <p>Hola,  ".$row['pnUsuario']." ". $row['paUsuario'] ."</p>
+        <p>El usuario ".$row['pnFavorito']. " ". $row['paFavorito']." ha publicado un nuevo anuncio.</p> 
+        
+        <p>Revisa Publitodo para mantenerte al dia de tus publicaciones favoritas.</p>
+
+        <br>
+        <p>Atentamente, Publitodo</p>
+        ";
+
+        try {
+    
+            //direccion de quien envia el correo, en este caso nuestra cuenta
+            $mail->setFrom('publitodo.2020@gmail.com', 'Publitodo');
+    
+            //direccion de a quien se envia el correo, en este caso seria el correo que el usuario registro
+            $mail->addAddress($row['correoUsuario'], $row['pnUsuario']." ".$row['paUsuario']);//TODO:Poner correo de usuario
+    
+            //La razon del correo
+            $mail->Subject = 'Notificacion de nueva publicacion.';
+    
+            //aqui se pone el cuerpo del email, es en codigo html, una simple cadena html
+            $mail->Body = $cuerpo_mail;
+        
+           /* Parametros SMTP. */
+           /* Le dice a  PHPMailer que use SMTP. */
+            $mail->isSMTP();
+        
+           /* Direccion del servidor de SMTP, en este caso uso el de google. */
+            $mail->Host = 'smtp.gmail.com';
+    
+           /* Usar SMTP autenticacion. */
+            $mail->SMTPAuth = TRUE;
+        
+           /* Configura el sistema de encriptacion. */
+            $mail->SMTPSecure = 'ssl';//tls
+        
+    
+           /* Aqui va el usuario de la cuenta de google del proyecto. */
+            $mail->Username = 'publitodo.2020@gmail.com';
+        
+           /* La contrasena de la cuenta. */
+            $mail->Password = 'Publitodo_2020.';
+        
+           /* El puerto en el que esta el servidor SMTP de Google. */
+            $mail->Port = 465;
+    
+            //le dice la extension  que el cuerpo del mail es html
+            $mail-> IsHTML(true);
+        
+           /* Envia el correo */
+            $mail->send();
+        }
+        catch (Exception $e)
+        {
+            echo $e->errorMessage();
+        }
+        catch (\Exception $e)
+        {
+            echo $e->getMessage();
+        }
+
+
+    //echo $row['name'];
+    }
 
 break;
 
